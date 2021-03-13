4e7f317b1348e0378fc2bc76ef062cb18d00967d
MHwishes <1025383143@qq.com>
2021-03-13 15:14:59 +0800

    add useEffect 原理

--- react-demos/src/App.js
+++ react-demos/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import { Demo1 as FakeUseState } from './components/FakeUseState.jsx'
+import { Demo1 as FakeUseState } from './components/FakeUseStateAndUseEffect.jsx'
 
 function App() {
   return (
     <>
-     <FakeUseState/>
+      <FakeUseState />
     </>
   );
 }
--- react-demos/src/components/FakeUseStateAndUseEffect.jsx
+++ react-demos/src/components/FakeUseStateAndUseEffect.jsx
@@ -13,6 +13,7 @@ function useState(initialState) {
     states[currenCursor] = newState;
     render();
     cursor = 0;
+    effectCursor = 0;
   }
 
   ++cursor;
@@ -20,9 +21,40 @@ function useState(initialState) {
   return [states[currenCursor], setState];
 }
 
+const allDeps = [];
+let effectCursor = 0;
+
+function useEffect(callback, deps) {
+  if (!allDeps[effectCursor]) {
+    allDeps[effectCursor] = deps;
+    ++effectCursor;
+    callback();
+    return;
+  }
+
+  const currenEffectCursor = effectCursor;
+  const rawDeps = allDeps[currenEffectCursor];
+  const isChanged = rawDeps.some((dep, index) => dep !== deps[index]);
+  if (isChanged) {
+    callback();
+  }
+  ++effectCursor;
+}
+
 export const Demo1 = () => {
   const [count1, setCount1] = useState(0);
   const [count2, setCount2] = useState(10);
+  const [count3, setCount3] = useState(100);
+
+  useEffect(() => {
+    console.log('count1 update: ', count1);
+  }, [count1]);
+
+  // 只会在compoentDidMount时，触发一次
+  // 副作用函数不会多次执行
+  useEffect(() => {
+    console.log('count3 update: ', count3);
+  }, [count3]);
 
   return (
     <div>
