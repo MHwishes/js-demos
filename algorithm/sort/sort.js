453d656a95768d2e99c767f1deb6022622527e15
MHwishies <1025383143@qq.com>
2021-06-20 14:22:29 +0800

    添加快排算法

--- algorithm/sort/sort.js
+++ algorithm/sort/sort.js
@@ -11,3 +11,34 @@ export const bubbleSort = (array) => {
     }
     return array;
 }
+
+// 快排 [2, 5, 1, 7, 3]
+export const quickSort = (array, left = 0, right) => {
+    right = typeof right != 'number' ? array.length - 1 : right;
+    if (left < right) {
+        let partitionIndex = partition(array, left, right);
+        quickSort(array, left, partitionIndex - 1);
+        quickSort(array, partitionIndex + 1, right);
+    }
+    return array;
+
+}
+
+const partition = (arr, left, right) => {
+    const pivot = left;
+    let index = pivot + 1;
+    for (let i = index; i <= right; i++) {
+        if (arr[i] < arr[pivot]) {
+            swap(arr, i, index)
+            index++;
+        }
+    }
+    swap(arr, pivot, index - 1);
+    return index - 1;
+
+}
+const swap = (arr, i, j) => {
+    var temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
--- algorithm/sort/sort.test.js
+++ algorithm/sort/sort.test.js
@@ -1,8 +1,15 @@
-import { bubbleSort } from './sort'
+import { bubbleSort, quickSort } from './sort'
 
 describe('bubbleSort', () => {
     it('should be sorted in asc order', () => {
         const res = bubbleSort([2, 5, 1, 7, 3]);
+        expect(res).toEqual([1, 2, 3, 5, 7])
+    })
+})
+
+describe('quickSort', () => {
+    it('should be sorted in asc order', () => {
+        const res = quickSort([2, 5, 1, 7, 3]);
         expect(res).toEqual([1, 2, 3, 5, 7])
     })
 })
