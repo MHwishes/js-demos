f4bad6ef0cdf24343c9d581a5f3e0f99b915e01e
MHwishies <1025383143@qq.com>
2021-08-09 23:10:47 +0800

    fix the arrow functions issue

--- debounce&throttle/debounce.js
+++ debounce&throttle/debounce.js
@@ -17,16 +17,14 @@ const debounce1 = (func, time) => {
 }
 // container.onmousemove = debounce1(getUserAction, 1000);
 
-//第二版 修改this 的指向,为什么使用apply 后this 依旧还是指window 对象？
+//第二版 修改this 的指向,使用apply 后this 指向 <div id="container-debounce"/>
 const debounce2 = (func, time) => {
     let timeout;
-    return () => {
+    return function () {
         let context = this;
         clearTimeout(timeout)
-        timeout = setTimeout(() => {
+        timeout = setTimeout(function () {
             func.apply(context)
-            console.log(context, '函数里context')
-            console.log(context, "context2")
         }, time)
     }
 }
@@ -35,19 +33,15 @@ const debounce2 = (func, time) => {
 // 第二版的问题：如果我们不使用 debouce 函数，这里会打印 event 是MouseEvent 对象,
 // 但是在我们实现的 debounce 函数中，却只会打印 undefined!
 
-// 第三版
-const debounce3 = (func, time) => {
+// 第三版 修改event对象的指向
+function debounce3(func, time) {
     let timeout;
     return () => {
         let context = this;
-        var args = arguments;
-        console.log(args, "args")
+        let args = arguments;
         clearTimeout(timeout)
         timeout = setTimeout(() => {
             func.apply(context, args)
-            console.log(context, '函数里context')
-            console.log(context, "context2")
-            console.log(args, "args")
         }, time)
     }
 }
