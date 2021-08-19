1719e0f58d2bcfa340b26e75290b00d02cbc1657
MHwishies <1025383143@qq.com>
2021-08-19 21:25:26 +0800

    add setState 机制 demo

--- react-demos/src/App.js
+++ react-demos/src/App.js
@@ -1,11 +1,14 @@
+import React from 'react'
 import './App.css';
+import ExampleSetState from './components/ExampleSetState'
 import { Demo1 as FakeUseState } from './components/FakeUseStateAndUseEffect.jsx'
 
 function App() {
   return (
-    <>
+    <div>
       <FakeUseState />
-    </>
+      <ExampleSetState />
+    </div>
   );
 }
 
